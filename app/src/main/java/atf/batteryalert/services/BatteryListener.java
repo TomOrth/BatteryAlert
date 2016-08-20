e94a1552856475aa94166178629aa27729ae3f1f
atf1999 <torth212@gmail.com>
2016-08-19 20:46:58 -0400

    Created notification logic. Hoping to test tomorrow

--- app/src/main/java/atf/batteryalert/services/BatteryListener.java
+++ app/src/main/java/atf/batteryalert/services/BatteryListener.java
@@ -1,12 +1,18 @@
 package atf.batteryalert.services;
 
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+
+import atf.batteryalert.R;
+import atf.batteryalert.utils.PreferenceUtil;
 
 /**
  * Created by tom on 8/17/16.
@@ -16,6 +22,21 @@ public class BatteryListener extends Service {
         @Override
         public void onReceive(Context context, Intent intent) {
             //TODO: add notification code
+            int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+            if(level <= PreferenceUtil.getLevel(context)) {
+                NotificationCompat.Builder notify = new NotificationCompat.Builder(context)
+                        .setContentTitle("CHARGE PHONE")
+                        .setContentText("Your phone has reached your desired limit. You should charge your phone now")
+                        .setSmallIcon(R.mipmap.ic_launcher);
+
+                // Sets an ID for the notification
+                int mNotificationId = 001;
+                // Gets an instance of the NotificationManager service
+                NotificationManager mNotifyMgr =
+                        (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                // Builds the notification and issues it.
+                mNotifyMgr.notify(mNotificationId, notify.build());
+            }
         }
     };
     @Nullable
